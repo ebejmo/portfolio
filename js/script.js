266044f63592e612582b83b5446dff5a0218ef1b
ebejmo <94139531+ebejmo@users.noreply.github.com>
2023-05-31 23:12:28 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -37,3 +37,37 @@ popupCards.forEach((close) => {
     popupContainer.style.display = "none";
   };
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  var mailIcon = document.getElementById("mail-icon");
+  var popup = document.getElementById("pop-up");
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var messageInput = document.getElementById("message");
+  var emailForm = document.getElementById("email-form");
+
+  mailIcon.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.remove("hidden");
+  });
+
+  emailForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var message = messageInput.value;
+
+    var recipient = "bejmoemil@gmail.com";
+    var subject = "Email from " + name;
+    var body = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+    var mailtoLink =
+      "mailto:" +
+      recipient +
+      "?subject=" +
+      encodeURIComponent(subject) +
+      "&body=" +
+      encodeURIComponent(body);
+
+    window.location.href = mailtoLink;
+  });
+});
